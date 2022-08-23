54dbc67574d2675cb33dfdc37a6dcb83ad1352b3
Moritz1404 <moritzsteinbusch@gmail.com>
2022-08-23 10:51:27 +0200

    as

--- .github/validateXML.py
+++ .github/validateXML.py
@@ -31,7 +31,7 @@ print(rootdir)
 for file in CHANGED_FILES.split():
     if not re.match('^[a-z0-9-]*\.xml?$', file.split('/')[-1]):
         #print("Invalid file name!")
-        print('echo "::error::Filename $filename doesnt match possible files" && exit 1')
+        print('::error::Filename $filename doesnt match possible files && exit 1')
         #sys.exit(1)
 
 #Durchlaueft den aktuellen Firmenordner und checkt alle .xml Dateien
