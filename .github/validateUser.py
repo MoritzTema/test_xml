2262b5724d79c3571fde43ad342605f8f5f3e95a
Moritz1404 <moritzsteinbusch@gmail.com>
2022-10-04 10:10:53 +0200

    asd

--- .github/validateUser.py
+++ .github/validateUser.py
@@ -27,7 +27,7 @@ currentCompany = CHANGED_FILES.split()[0].split('/')[0]
 
 #Checken, ob in nur einem Pfad Aenderungen vorgenommen wurden
 for file in CHANGED_FILES.split():
-    if file.split('/')[1] == 'index.xml':
+    if file.split('/')[0] == 'index.xml':
         print("index.xml will be ignored")
         pass
     elif currentCompany == file.split('/')[0]:
