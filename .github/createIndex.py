a7f9bd15f9dbac3969e2392e3eeb1aa850359f35
Moritz1404 <moritzsteinbusch@gmail.com>
2022-10-04 11:24:44 +0200

    asd

--- .github/createIndex.py
+++ .github/createIndex.py
@@ -27,12 +27,8 @@ def createXML(paths, IDs):
 
     xml_str = root.toprettyxml(indent = "\t")
 
-    print("Hallo")
 
     with open('index.xml') as oldIndex:
-        print("oldIndex: " + oldIndex.read())
-        print("xml_str: " + xml_str)
-        print("In open")
         if xml_str == oldIndex.read():
             sys.exit(0)
         else:        
