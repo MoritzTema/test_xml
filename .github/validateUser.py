69dbb5d4922b6a53d283e9def1801f3fd8f41b07
Moritz1404 <moritzsteinbusch@gmail.com>
2022-10-10 14:38:10 +0200

    asd

--- .github/validateUser.py
+++ .github/validateUser.py
@@ -6,7 +6,7 @@ CHANGED_FILES = os.environ.get("CHANGED_FILES")
 CURRENT_USER = os.environ.get("CURRENT_USER")
 ALLOWED_USERS = os.environ.get("ALLOWED_USERS")
 
-print(CHANGED_FILES.split().__len__)
+print(len(CHANGED_FILES.split()))
 
 allowedUsersReadable = json.loads(ALLOWED_USERS)
 
