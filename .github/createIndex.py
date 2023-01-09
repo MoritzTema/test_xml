18644ee770ee349d9a2b85d47e0f46f58d150f79
Moritz1404 <moritzsteinbusch@gmail.com>
2023-01-09 11:39:35 +0100

    trim path in index.xml

--- .github/createIndex.py
+++ .github/createIndex.py
@@ -63,7 +63,8 @@ for subdir, dirs, files in os.walk(rootDir, topdown=True):
 
         #Alle Pfade und productIDs werden gesammelt, um sie spaeter in den Index zu schreiben
         #paths[0] = productIDs[0] usw. (Zugehoerige Werte)
-        paths.append(filepath)
+        #'../test_xml/' wird nicht mit in die index.xml geschrieben 
+        paths.append(os.path.relpath(filepath, '../test_xml/..'))
         productIDs.append(productID)
         
 createXML(paths, productIDs)
